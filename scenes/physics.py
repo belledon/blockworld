45825a016a3a49613bc34a1ca1f68d3aab2e42a5
belledon <belledon@mit.edu>
2018-08-03 11:24:04 -0400

    improved run_test, implementing stability assessment

--- scenes/physics.py
+++ scenes/physics.py
@@ -1,4 +1,6 @@
-
+import pprint
+import numpy as np
+from . import block_scene
 
 class TowerTester:
 
@@ -25,38 +27,51 @@ class TowerTester:
     def mat_ps(self):
         return self._map_ps
 
+    #-------------------------------------------------------------------------#
 
-    def configurations(self):
+    # Material assignment and sampling
+
+    def sample_tower(self):
+        """
+        Randomly assigns materials to a tower structure.
+        """
+        temp_tower = copy.copy(self.structure)
+        blocks = temp_tower['nodes']
+        n_blocks = len(blocks)
+        materials = np.random.choice([1, 0],
+                                     size = n_blocks,
+                                     p = self.mat_ps)
+
+        blocks = apply_feature(blocks, 'material', materials)
+        blocks = apply_feature(blocks, 'congruency', bool(np.ones(n_blocks)))
+        temp_tower['nodes'] = blocks
+        return temp_tower
+
+
+    def configurations(self, tower):
         """
         Generator for different tower configurations.
         """
-        tower = self.structure
         n_blocks = len(tower['nodes']) - 1
+        materials = extract_feature(tower['nodes'], 'material')
+        congruency = extract_feature(tower['nodes'], 'congruency')
 
         for ind in range(n_blocks):
-
             temp_tower = copy.copy(tower)
             blocks = temp_tower['nodes']
-            materials = np.random.choice([1, 0],
-                                         size = n_blocks,
-                                         p = self.mat_ps)
-
-            for b_i, block in enumerate(blocks):
-                material = materials[b_i]
-                block['material'] = material
-                if int(block['id']) == (ind + 1):
-                    block['appearance'] = self.materials[1 - orig_material]
-
-                else:
-                    block['appearance'] = self.materials[materials[b_i]]
-
+            temp_con = copy.copy(congruency)
+            temp_con[ind] = False
             yield (ind, temp_tower)
+
+    #-------------------------------------------------------------------------#
+
+    # Physics and movement
 
     def simulate(self, tower):
         """
         Controls simulations and extracts positions
         """
-        scene = BlockScene(tower, frames = 10)
+        scene = block_scene.BlockScene(tower, frames = 10)
         scene.bake_physics()
         trace = scene.get_trace(frames = [0, 10])
         return [t['position'] for t in trace]
@@ -65,13 +80,47 @@ class TowerTester:
         vel = (positions[-1] - positions[0]) / len(positions)
         return np.sum(np.abs(vel)) > eps
 
+    #-------------------------------------------------------------------------#
+
     def __call__(self):
         """
         Evaluates the stability of the tower at each block.
+
+        Returns:
+          - The randomly sampled congruent tower.
+          - The stability results for each block in the tower.
         """
+        base_tower = self.sample_tower()
         d = {}
-        for (block_id, tower) in self.configurations():
+        for (block_id, tower) in self.configurations(base_tower):
             frames = self.simulate(tower)
             d.update({block_id : self.movement(frames)})
 
-        return d
+        return base_tower, d
+
+
+# Helper functions
+
+def extract_feature(blocks, feature):
+    n_blocks = len(blocks)
+    values = []
+    for i in range(n_blocks):
+        values.append(blocks['{0:d}'.format(i)[feature])
+    return values
+
+def apply_feature(blocks, feature, values):
+    """
+    Applys a feature to a set of blocks in a tower
+    """
+    n_blocks = len(blocks)
+    n_values = len(values)
+    if n_blocks != n_values:
+        raise ValueError('Block, values missmatch')
+
+    pprint.pprint(blocks)
+    for b_i in n_blocks:
+        block = blocks['{0:d]}'.format(b_i)]
+        block[feature] = values[b_i]
+    pprint.pprint(blocks)
+
+    return blocks
--- tests/run_test.sh
+++ tests/run_test.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-ROOT="$(dirname $DIR)"
+. /etc/os-release
+OS=$NAME
+
+ROOT="$( git rev-parse --show-toplevel )"
+if [ "$OS" = "CentOS Linux" ]; then
+    ROOT="$( echo ${ROOT#/net/storage001.ib.cluster})"
+    module add openmind/singularity
+fi
+
+BASE="$(echo "$ROOT" | cut -d "/" -f2)"
 export PYTHONPATH=$ROOT
-SCRIPT="${ROOT}/tests/$1"
+PREFIX="$( git rev-parse --show-prefix )"
+SCRIPT="${ROOT}/${PREFIX}/$1"
 shift
 
 CONT="${ROOT}/singularity/env.simg"
 
-singularity exec $CONT python3 $SCRIPT "$@"
+singularity exec -B "/$BASE:/$BASE" $CONT python3 $SCRIPT "$@"
 
 
