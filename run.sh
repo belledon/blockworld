039f9be5ed45f2837466b1764fd5c47194343e7a
belledon <belledon@mit.edu>
2018-08-28 20:07:29 -0400

    added setup and config workflow

---
+++ run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+. /etc/os-release
+OS=$NAME
+
+ROOT="$( git rev-parse --show-toplevel )"
+if [ "$OS" = "CentOS Linux" ]; then
+    ROOT="$( echo ${ROOT#/net/storage001.ib.cluster})"
+    module add openmind/singularity
+fi
+
+BASE="$(echo "$ROOT" | cut -d "/" -f2)"
+export PYTHONPATH=$ROOT
+SCRIPT="$1"
+shift
+
+CONT="${ROOT}/singularity/env.simg"
+
+singularity exec -B "/$BASE:/$BASE" $CONT $SCRIPT "$@"
+
+
