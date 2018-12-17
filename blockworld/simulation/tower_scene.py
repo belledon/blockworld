edd037686a0a9306998ac5d9e3ad1b17850fb152
belledon <mbelledonne@gmail.com>
2018-12-17 17:29:39 -0500

    updated simulation parameters, TODO: make arguments

--- blockworld/simulation/tower_scene.py
+++ blockworld/simulation/tower_scene.py
@@ -41,6 +41,9 @@ class TowerPhysics:
     def __init__(self, tower_json, loader = Loader()):
         self.loader = loader
         self.description = tower_json
+        p = bc.BulletClient(connection_mode=pybullet.DIRECT)
+        p.resetSimulation()
+        self.physicsClient = p
 
     #-------------------------------------------------------------------------#
     # Attributes
@@ -76,20 +79,28 @@ class TowerPhysics:
     # Methods
 
     def __enter__(self):
-        p = bc.BulletClient(connection_mode=pybullet.DIRECT)
-        p.resetSimulation()
-        self.physicsClient = p
+        # p = bc.BulletClient(connection_mode=pybullet.DIRECT)
+        # self.physicsClient.resetSimulation()
+        # self.physicsClient = p
         self.world = self.description
         return self
 
     def __exit__(self, *args):
-        del self.physicsClient
-        self.physicsClient = None
+        # del self.physicsClient
+        # self.physicsClient = None
+        self.physicsClient.resetSimulation()
 
     def get_trace(self, frames, objects, time_step = 120, fps = 60):
-        """
-        Obtains world state for select frames.
+        """Obtains world state from simulation.
+
         Currently returns the position of each rigid body.
+        The total duration is equal to `frames / fps`
+
+        Arguments:
+            frames (int): Number of frames to simulate.
+            objects ([str]): List of strings of objects to report
+            time_step (int, optional): Number of physics steps per second
+            fps (int, optional): Number of frames to report per second.
         """
         for obj in objects:
             if not obj in self.world.keys():
@@ -99,24 +110,24 @@ class TowerPhysics:
         p.setGravity(0,0,-10)
         p.setPhysicsEngineParameter(
             fixedTimeStep = 1.0 / time_step,
-            numSolverIterations = 200,
+            numSolverIterations = 100,
             enableConeFriction = 0,
         )
 
         positions = np.zeros((frames, len(objects), 3))
         rotations = np.zeros((frames, len(objects), 4))
 
-        phys_step_per_frame = time_step / fps
-        dur = int(phys_step_per_frame * frames)
+        steps_per_frame = int(time_step / fps)
+        dur = int(max(1, ((frames / fps) * time_step)))
         for f in range(dur):
             p.stepSimulation()
 
-            if f % phys_step_per_frame != 0:
+            if f % steps_per_frame != 0:
                 continue
             for c, obj in enumerate(objects):
                 obj_id = self.world[obj]
                 pos, rot = p.getBasePositionAndOrientation(obj_id)
-                frame = int(f / phys_step_per_frame)
+                frame = int(f / steps_per_frame)
                 positions[frame, c] = np.array(pos).flatten()
                 rotations[frame, c] = np.array(rot).flatten()
 
